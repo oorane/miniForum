1eedd2d9220ef93f53534e392cf7e4e44a442144
Steffie <steffiehu23@gmail.com>
2021-06-17 15:48:49 +0200

    Correction Sujet

--- classes.js
+++ classes.js
@@ -1,14 +1,26 @@
-class Topic {
+class Sujet {
 
-    constructor(id, title, date, author_id){
-        this.id=id;
+    constructor(id, title, date, author_id) {
+        this.id = id;
         this.title = title;
         this.date = date;
-        this.author_id=author_id;
+        this.author_id = author_id;
     };
 
-    displayTopic(){
-        console.log(`Topic n° ${id}: ${title} crée le ${date} par ${author_id}.`);
-    }; 
-    
+    displaySujet() {
+        console.log(`Topic n° ${id}: ${title} crée le ${date} par ${author_id.username}.`);
+    };
+
+    infoSujet() {
+
+        var newDate = new Date(date * 1000);
+        var year = date.getFullYear();
+        var month = date.getMonth();
+        var day = date.getDay();
+        var hour = date.getHours();
+        var minute = date.getMinutes();
+        var seconds = date.getSeconds();
+        return  `Posté le ${newDate.day}/${newDate.month}/${newDate.year} à ${newDate.hour}:${newDate.minute} par ${this.author_id.username}`;
+
+    }
 }
