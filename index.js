e98aefaeae81fa55214d6b5b06fc7af6547fb76c
Thomas Boulez <thomas@boulez.com>
2021-06-17 16:59:26 +0200

    Création des boutons du header

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
- 
+const header = document.getElementById("h");
+
+const hBoutons = [document.getElementById("hBouton1"), document.getElementById("hBouton2"), document.getElementById("hBouton3")];
+let Selected = 0;
+
+console.log(header);
+
+hBoutons.forEach((element, index) => {
+
+    if (index === Selected) {
+        element.setAttribute("class", "headerB hSelected");
+    } else {
+        element.setAttribute("class", "headerB hUnselected");
+    }
+
+
+    element.addEventListener("click", event => {
+        Selected = index;
+        hBoutons.forEach((element2, index2) => {
+            if (index2 === Selected) {
+                element2.setAttribute("class", "headerB hSelected");
+            } else {
+                element2.setAttribute("class", "headerB hUnselected");
+            }
+        });
+    });
+
+    element.addEventListener("mouseover", event => {
+        element.setAttribute("class", "headerB hHover");
+    });
+
+    element.addEventListener("mouseout", event => {
+        if (index === Selected) {
+            element.setAttribute("class", "headerB hSelected");
+        } else {
+            element.setAttribute("class", "headerB hUnselected");
+        }
+    });
+});
+
+montrerBoutonAccueil();
+montrerBoutonMMC();
+montrerBoutonSD();
+
+
+
+function montrerBoutonAccueil() {
+    hBoutons[0].textContent = "Accueil";
+}
+
+function montrerBoutonMMC() {
+    hBoutons[1].textContent = "Modifier mon compte";
+}
+
+function montrerBoutonCUC() {
+    hBoutons[1].textContent = "Créer un compte";
+}
+
+function montrerBoutonSD() {
+    hBoutons[2].textContent = "Se déconnecter";
+}
+
+function montrerBoutonSC() {
+    hBoutons[2].textContent = "Se connecter";
+}
